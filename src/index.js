39ac601145af21841e124217baeb8c6c1a263a89
Catherine Bronzo <cbronzo@mac.com>
2021-02-07 04:11:11 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  const form = document.getElementById("create-task-form");
+  const ul = document.getElementById("tasks");
+  
+  
+  
+  
+  
+  
 });
