43fabe04ee7a5e34d7f78eb3cabffa22fe3fd851
Catherine Bronzo <cbronzo@mac.com>
2021-02-07 04:11:20 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("create-task-form");
   const ul = document.getElementById("tasks");
   
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
   
   
   
