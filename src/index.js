1f383a97d8f7a21134bc842638c4972bc883eb8a
Catherine Bronzo <cbronzo@mac.com>
2021-02-07 04:11:39 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,9 +10,8 @@ document.addEventListener("DOMContentLoaded", () => {
     let description = document.getElementById("new-task-description").value;
     let text = document.createTextNode(description);
     let newLi = document.createElement("li");
-  
-  
-  
-  
+    let li = newLi.appendChild(text);
+    ul.appendChild(li);
+
   
 });
